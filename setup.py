d62b71ae33215721cbeac81784ec4f290c64b852
Koen van Eijk <vaneijk.koen@gmail.com>
2024-06-07 16:34:24 +0200

    Version increment

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ if current_os and current_os in extras_require:
 
 setup(
     name="OpenRecall",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=install_requires,
     long_description=long_description,
