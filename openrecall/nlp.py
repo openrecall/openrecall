9d3ebcb2a2564b870326130be53855e21b5ea2b1
Soren L. Hansen <sorenisanerd@gmail.com>
2024-07-23 14:02:34 -0700

    Create local model cache

--- openrecall/nlp.py
+++ openrecall/nlp.py
@@ -1,9 +1,21 @@
 import numpy as np
+import os
+import os.path
 from sentence_transformers import SentenceTransformer
+from openrecall.config import model_cache_path
+
+def get_model(model_name):
+    cache_path = os.path.join(model_cache_path, model_name)
+    if os.path.isdir(cache_path):
+        return SentenceTransformer(cache_path)
+    else:
+        model = SentenceTransformer(model_name)
+        model.save(cache_path)
+        return model
 
 
 def get_embedding(text):
-    model = SentenceTransformer("all-MiniLM-L6-v2")
+    model = get_model("all-MiniLM-L6-v2")
     sentences = text.split("\n")
     sentence_embeddings = model.encode(sentences)
     mean = np.mean(sentence_embeddings, axis=0)
